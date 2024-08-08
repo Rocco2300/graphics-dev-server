29d34cb3ff8ddd3099f7c2dbfeb7dee77fb1f024
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2024-08-08 11:24:34 +0300

    add libav and use shared memory to save screen data

--- src/server.cpp
+++ src/server.cpp
@@ -1,7 +1,13 @@
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <chrono>
 
 #include <windows.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
 
 static constexpr char dllName[] = "C:/Users/grigo/Repos/graphics-dev-server/bin/libshim.dll";
 static constexpr unsigned int dllNameLen = sizeof(dllName) + 1;
@@ -31,6 +37,24 @@ int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prevInst, PSTR cmdline, int cmdsh
                         &si,
                         &pi)) {
         std::cerr << "Failed to launch the program " << program << '\n';
+        return 1;
+    }
+
+    HANDLE mapFile = CreateFileMapping(INVALID_HANDLE_VALUE,
+                                       nullptr,
+                                       PAGE_READWRITE,
+                                       0,
+                                       1024 * 1024 * 12,
+                                       "Local\\VideoFrame");
+    if (!mapFile) {
+        std::cerr << "Could not create file mapping for the video frame\n";
+        std::cerr << GetLastError() << '\n';
+        return 1;
+    }
+    auto buffer = MapViewOfFile(mapFile, FILE_MAP_ALL_ACCESS, 0, 0, 1024 * 1024 * 12);
+    if (!buffer) {
+        std::cerr << "Error allocating buffer\n";
+        return 1;
     }
 
     LPVOID remoteBuffer = VirtualAllocEx(pi.hProcess,
@@ -40,10 +64,12 @@ int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prevInst, PSTR cmdline, int cmdsh
                                          PAGE_EXECUTE_READWRITE);
     if (!remoteBuffer) {
         std::cerr << "Error allocating memory in remote process\n";
+        return 1;
     }
 
     if (!WriteProcessMemory(pi.hProcess, remoteBuffer, dllName, dllNameLen, nullptr)) {
         std::cerr << "Could not write memory into remote process\n";
+        return 1;
     }
 
     auto threadHandle = CreateRemoteThread(pi.hProcess,
@@ -55,7 +81,40 @@ int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prevInst, PSTR cmdline, int cmdsh
                                            nullptr);
     if (!threadHandle) {
         std::cerr << "Could not launch remote thread\n";
+        return 1;
     }
+
+    HANDLE event = CreateEvent(nullptr, FALSE, FALSE, "Local\\VideoFrameReady");
+    if (!event) {
+        std::cerr << "Failed to create event\n";
+        return 1;
+    }
+
+    std::ofstream out("C:/Users/grigo/Desktop/test.ppm");
+    DWORD result = WaitForSingleObject(event, INFINITE);
+    if (result == WAIT_OBJECT_0) {
+        auto s = std::chrono::high_resolution_clock::now();
+        auto* data = (uint8_t*) buffer;
+        //out.write((char*)buffer + 8, *(size_t*)buffer);
+        out << "P3\n";
+        out << 1280 << ' ' << 720 << '\n';
+        out << "255\n";
+        for (int i = 0; i < 720; i++) {
+            for (int j = 0; j < 1280; j++) {
+                int index      = (i * 1280 + j) * 3;
+                uint8_t* pixel = &data[index];
+                out << static_cast<int>(pixel[0]) << ' ' << static_cast<int>(pixel[1]) << ' '
+                    << static_cast<int>(pixel[2]) << '\n';
+            }
+        }
+        auto e = std::chrono::high_resolution_clock::now();
+
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count()
+                  << '\n';
+        //out.write((char*)buffer, 1024 * 1024 * 12);
+    }
+
+    ResetEvent(event);
 
     WaitForSingleObject(pi.hProcess, INFINITE);
 
--- src/shim.cpp
+++ src/shim.cpp
@@ -1,19 +1,25 @@
-#include "MinHook.h"
+#include <MinHook.h>
 
 #include <gl/gl.h>
 #include <windows.h>
 
+#include <chrono>
 #include <cinttypes>
+#include <cstring>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <vector>
-#include <chrono>
 
 typedef BOOL(WINAPI* SwapBuffersType)(HDC hdc);
 
 SwapBuffersType OriginalSwapBuffers = nullptr;
 
 static bool saved{};
+static DWORD error{};
+static void* buffer{};
+static HANDLE event{};
+static HANDLE mapFile{};
 static std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<double>> start;
 
 BOOL WINAPI DetourSwapBuffers(HDC hdc) {
@@ -23,26 +29,36 @@ BOOL WINAPI DetourSwapBuffers(HDC hdc) {
         glGetIntegerv(GL_VIEWPORT, viewport);
         int width  = viewport[2];
         int height = viewport[3];
-        std::vector<uint8_t> pixels(3 * width * height);
-        glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels.data());
+        //std::vector<uint8_t> pixels(3 * width * height);
+        glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, buffer);
 
-        auto now = std::chrono::high_resolution_clock::now();
+        auto now  = std::chrono::high_resolution_clock::now();
         auto time = std::chrono::duration_cast<std::chrono::seconds>(now - start);
         if (!saved && time.count() >= 5.0) {
-            MessageBoxA(nullptr, "Saved a screen capture", "Message", MB_OK);
-            std::ofstream out("C:\\Users\\grigo\\Desktop\\test.ppm");
-            out << "P3\n";
-            out << width << ' ' << height << '\n';
-            out << "255\n";
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    int index      = (i * width + j) * 3;
-                    uint8_t* pixel = &pixels[index];
-                    out << static_cast<int>(pixel[0]) << ' ' << static_cast<int>(pixel[1]) << ' '
-                        << static_cast<int>(pixel[2]) << '\n';
-                }
-            }
-            out.close();
+            //std::stringstream data;
+
+            auto s = std::chrono::high_resolution_clock::now();
+            //for (int i = 0; i < height; i++) {
+            //    for (int j = 0; j < width; j++) {
+            //        int index      = (i * width + j) * 3;
+            //        uint8_t* pixel = &pixels[index];
+            //        data << pixel[0] << pixel[1] << pixel[2];
+            //    }
+            //}
+
+            //const size_t bufferSize = 1024 * 1024 * 12;
+
+            //auto dataString  = data.str();
+            //char* dataBuffer = dataString.data();
+            //size_t length    = dataString.length();
+            //memcpy(buffer, &length, sizeof(length));
+            //memcpy((char*) buffer + 8, dataBuffer, dataString.length());
+            SetEvent(event);
+
+            auto e = std::chrono::high_resolution_clock::now();
+
+            std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(e - s).count()
+                      << '\n';
             saved = true;
         }
     } else {
@@ -54,13 +70,31 @@ BOOL WINAPI DetourSwapBuffers(HDC hdc) {
 BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
     switch (reason) {
     case DLL_PROCESS_ATTACH:
+        MessageBox(nullptr, "Message", "Message", MB_OK);
         start = std::chrono::high_resolution_clock::now();
+
+        mapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "Local\\VideoFrame");
+        if (!mapFile) {
+            std::cerr << "Error opening memory mapped file\n";
+            return 1;
+        }
+
+        buffer = MapViewOfFile(mapFile, FILE_MAP_ALL_ACCESS, 0, 0, 1024 * 1024 * 12);
+
+        event = OpenEvent(EVENT_MODIFY_STATE, FALSE, "Local\\VideoFrameReady");
+        if (!event) {
+            std::cerr << "Error opening frame ready event\n";
+            return 1;
+        }
+
         if (MH_Initialize() != MH_OK) {
             std::cerr << "Could not init minhook\n";
             return 1;
         }
 
-        if (MH_CreateHookApi(L"gdi32", "SwapBuffers", (void*) &DetourSwapBuffers,
+        if (MH_CreateHookApi(L"gdi32",
+                             "SwapBuffers",
+                             (void*) &DetourSwapBuffers,
                              reinterpret_cast<LPVOID*>(&OriginalSwapBuffers))) {
             std::cerr << "Could not create hook for swapBuffers\n";
             return 1;
@@ -71,7 +105,6 @@ BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
             return 1;
         }
 
-
         break;
     case DLL_PROCESS_DETACH:
         if (MH_DisableHook(MH_ALL_HOOKS) != MH_OK) {
@@ -81,6 +114,8 @@ BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
         if (MH_Uninitialize() != MH_OK) {
             return 1;
         }
+
+        CloseHandle(mapFile);
         break;
     }
     return TRUE;
